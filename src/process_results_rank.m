80677e9e68f791fed4901af93eb000dae6c7e5b1
Nicholas Molyneaux <nicholas.molyneaux@epfl.ch>
2016-01-03 09:38:07 +0100

    advanced results

--- src/process_results_rank.m
+++ src/process_results_rank.m
@@ -8,13 +8,13 @@ base_name_end = {'.csv_rank_'};
 for i=1:length(nodes)
     time = [];
     for j = 0:(nodes(i)-1)
-        filename = strcat(base_name,num2str(nodes(i)),base_name_end,num2str(j),'.csv');        
+        filename = strcat(base_name,num2str(nodes(i)),base_name_end,num2str(j),'.csv');
         data = dlmread(char(filename),',',0,1);
         time = [time,data];
-        if (j == 0 )
-            disp('rank 0');
-            data(end)
-        end
+        %if (j == 0 )
+        %    disp('rank 0');
+        %    data(end)
+        %end
     end    
     nodes(i)
     mean(time,2)
