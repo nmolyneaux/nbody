980b3918657003605fc733231a0c91bf7210502c
Nicholas Molyneaux <nicolas.molyneaux@gmail.com>
2016-01-09 13:48:06 +0100

    report with all figures

--- src/plot_collision_times.m
+++ src/plot_collision_times.m
@@ -21,3 +21,25 @@ plot(transpose(1:(size(time,1)-1)),time(1:end-1,2),'Color',[0.8500    0.3250    
 plot(transpose(1:(size(time,1)-1)),time(1:end-1,3),'Color',[0.9290    0.6940    0.1250])
 plot(transpose(1:(size(time,1)-1)),time(1:end-1,4),'Color',[0.4940    0.1840    0.5560])
 hold off;
+
+%%
+figure();
+hold on;
+plot(transpose(1:(size(time,1)-1)),smooth(time(1:end-1,1)),'Color',[0    0.4470    0.7410])
+plot(transpose(1:(size(time,1)-1)),smooth(time(1:end-1,2)),'Color',[0.8500    0.3250    0.0980])
+plot(transpose(1:(size(time,1)-1)),smooth(time(1:end-1,3)),'Color',[0.9290    0.6940    0.1250])
+plot(transpose(1:(size(time,1)-1)),smooth(time(1:end-1,4)),'Color',[0.4940    0.1840    0.5560])
+hold off;
+
+
+%%
+A_full = [transpose(1:1:8000),(time(:,1)),(time(:,2)),(time(:,3)),(time(:,4)),];
+idx = 1:4:8000;
+A = A_full(idx,:);
+plot(A(:,1),A(:,2))
+%dlmwrite('../figures/timing_two_nodes_for_plot.csv',A,',');
+
+%%
+B = A_full(4500:6000,1:3)
+plot(B(:,1),B(:,3))
+%dlmwrite('../figures/timing_two_masses_zoom_4500_6000',B,',');
--- src/process_results_rank.m
+++ src/process_results_rank.m
@@ -2,8 +2,10 @@ clear; close all; clc;
 
 nodes = [1,2,4,8,12,16,20,24,28,32];
 
-base_name = {'../results/qt-100000_timing_nodes_'}
+base_name = {'../results/bf-1000000_timing_nodes_'}
 base_name_end = {'.csv_rank_'};
+
+computeTimes = [];
 
 for i=1:length(nodes)
     time = [];
@@ -16,8 +18,29 @@ for i=1:length(nodes)
         %    data(end)
         %end
     end    
+    computeTimes = [computeTimes,nodes(i).*time(3,:)];
     nodes(i)
     mean(time,2)
     std(time,[],2)
     time
 end
+computeTimes = computeTimes./44875;
+%%
+
+nodesCS = cumsum(nodes);
+ymin = 0.75; ymax = 1.15;
+figure()
+hold on;
+for i = 1:length(nodes)
+    plot([nodesCS(i),nodesCS(i)],[ymin,ymax],'k')
+end
+scatter(1:147,computeTimes);
+ax = gca;
+ax.XTick = [0.5,2,5,11,21,35,53,75,101,131];
+ax.XTickLabel = {'1','2','4','8','12','16','20','24','28','32'};
+hold off;
+dlmwrite('../figures/computeTimesNormalizedBF10e6.csv',[1:147;computeTimes]')
+
+
+
+
